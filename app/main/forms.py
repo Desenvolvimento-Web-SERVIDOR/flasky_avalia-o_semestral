2f2f5f2fa601e0127f659be17b85110405eca484
Rogério Santos Barbosa <DevSemCash@gmail.com>
2025-11-28 20:04:03 -0300

    Add CursoForm for course registration

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,1 +1,11 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, TextAreaField
+from wtforms.validators import DataRequired, Length
 
+class CursoForm(FlaskForm):
+    nome = StringField('Qual é o nome do curso?', validators=[DataRequired()])
+    descricao = TextAreaField('Descrição (250 caracteres)', validators=[
+        DataRequired(), 
+        Length(max=250, message="Máximo de 250 caracteres")
+    ])
+    submit = SubmitField('Cadastrar')
