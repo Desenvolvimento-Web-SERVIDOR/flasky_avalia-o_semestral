bbcc74303cff3b8051b2f5a09f952a9538a3f3aa
Rogério Santos Barbosa <DevSemCash@gmail.com>
2025-11-28 20:03:44 -0300

    Update errors.py

--- app/main/errors.py
+++ app/main/errors.py
@@ -3,8 +3,6 @@ from . import bp
 
 @bp.app_errorhandler(404)
 def page_not_found(e):
-    # Em um cenário real, teríamos um template 404.html. 
-    # Aqui usamos o base com uma mensagem simples para não quebrar.
     return render_template('base.html'), 404
 
 @bp.app_errorhandler(500)
