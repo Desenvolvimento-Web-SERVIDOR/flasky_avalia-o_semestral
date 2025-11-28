a6c319b0191f96768346e13cba301951266ffd2c
Rogério Santos Barbosa <DevSemCash@gmail.com>
2025-11-28 19:52:41 -0300

    Initialize Flask application with Bootstrap and SQLAlchemy

---
+++ flasky.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from flask_sqlalchemy import SQLAlchemy
+from config import config
+
+bootstrap = Bootstrap()
+moment = Moment()
+db = SQLAlchemy()
+
+def create_app(config_name='default'):
+    app = Flask(__name__)
+    app.config.from_object(config[config_name])
+    config[config_name].init_app(app)
+
+    bootstrap.init_app(app)
+    moment.init_app(app)
+    db.init_app(app)
+
+    # Registro do Blueprint principal
+    from .main import bp as main_bp
+    app.register_blueprint(main_bp)
+
+    return app
